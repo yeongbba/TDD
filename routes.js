62b64ea6169ec4e6f79e1cd493988ab30c58e30e
yeongbba <yeongmolee2@gmail.com>
2020-11-26 15:39:21 +0900

    unit & int test update

--- routes.js
+++ routes.js
@@ -4,5 +4,7 @@ const productController = require("./controller/products");
 
 router.post("/", productController.createProduct);
 router.get("/", productController.getProducts);
+router.get("/:productId", productController.getProductById);
+router.put("/:productId", productController.updateProduct);
 
 module.exports = router;
--- test/integration/products.int.test.js
+++ test/integration/products.int.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const app = require("../../server");
 const newProduct = require("../data/new-product.json");
 
+let firstProduct;
 it("POST /api/products", async () => {
   const response = await request(app).post("/api/products").send(newProduct);
   expect(response.statusCode).toBe(201);
@@ -26,4 +27,35 @@ it("GET /api/products", async () => {
   expect(Array.isArray(response.body)).toBeTruthy();
   expect(response.body[0].name).toBeDefined();
   expect(response.body[0].description).toBeDefined();
+  firstProduct = response.body[0];
+});
+
+it("GET /api/product/:productId", async () => {
+  const response = await request(app).get("/api/products/" + firstProduct._id);
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe(firstProduct.name);
+  expect(response.body.description).toBe(firstProduct.description);
+});
+
+it("GET id doenst exist /api/products/:productId", async () => {
+  const response = await request(app).get(
+    "/api/products/5f5cb1f145b82ecaf43e3877"
+  );
+  expect(response.statusCode).toBe(404);
+});
+
+it("PUT /api/products", async () => {
+  const res = await request(app)
+    .put("/api/products/" + firstProduct._id)
+    .send({ name: "updated name", description: "updated desription" });
+  expect(res.statusCode).toBe(200);
+  expect(res.body.name).toBe("updated name");
+  expect(res.body.description).toBe("updated desription");
+});
+
+it("should return 404 on PUT /api/products", async () => {
+  const res = await request(app)
+    .put("/api/products" + "5f5d79abdc3acb1b95e0eb99")
+    .send({ name: "updated name", description: "updated desription" });
+  expect(res.statusCode).toBe(404);
 });
